4232e66f8824ff68edb48fa4d218a9d7777db5ac
cnmithun1 <cnmithun1@gmail.com>
2026-06-16 21:58:26 +0530

    introduced .env to secure password

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Set custom DNS servers to avoid potential DNS resolution issues
 const dns = require("dns");
@@ -6,9 +7,7 @@ const dns = require("dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://cnmithun1:cnmithun1@myfirstcluster.fltbk56.mongodb.net/devConnect",
-  );
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = { connectDB };
