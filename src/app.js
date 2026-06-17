7c06983a76bc67be8601768a3eb7e448467905dc
cnmithun1 <cnmithun1@gmail.com>
2026-06-17 18:58:42 +0530

    mongoose schema & signup api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,24 @@ const express = require("express");
 const app = express();
 const { adminAuth, userAuth } = require("./middlewares/auth");
 const { connectDB } = require("./config/database");
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Silpa Prajna",
+    lastName: "Nayak",
+    emailId: "silpa@example.com",
+    password: "password123",
+    gender: "Female",
+  });
+
+  try {
+    await user.save();
+    res.send("User created successfully");
+  } catch (err) {
+    res.status(500).send("Error creating user");
+  }
+});
 
 connectDB()
   .then(() => {
---
+++ src/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  emailId: String,
+  password: String,
+  gender: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
