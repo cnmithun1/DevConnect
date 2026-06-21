f0ffbb2cdd003570b1b76943138bbab857e874ab
cnmithun1 <cnmithun1@gmail.com>
2026-06-21 19:52:42 +0530

    get apis

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const User = require("./models/user");
 
 app.use(express.json());
 
+// signup
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
@@ -14,6 +15,51 @@ app.post("/signup", async (req, res) => {
     res.send("User created successfully");
   } catch (err) {
     res.status(500).send("Error creating user");
+  }
+});
+
+// get user by emailid
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      emailId: req.body.emailId,
+      firstName: req.body.firstName,
+    });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(500).send("Error finding user by emailId");
+  }
+});
+
+// get all users
+app.get("/getAllUsers", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("No users found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(500).send("Error finding users");
+  }
+});
+
+// get user by id
+app.get("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(500).send("Error finding user by Id");
   }
 });
 
